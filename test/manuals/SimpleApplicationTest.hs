6c03f48fc5a9fdaef631bcaeed792b0e9ab3ce73
Jan-Philip Loos <maxdaten@gmail.com>
2013-10-03 00:26:56 +0200

    added mouse callback for manual test

--- test/manuals/SimpleApplicationTest.hs
+++ test/manuals/SimpleApplicationTest.hs
@@ -19,11 +19,15 @@ main = do
             win <- createWindow 800 600 "test window"
             io $ print win
 
+            setCursorPosCallback win $ Just mousePos
             loop win
             --forever (return ())
             return 42
         loop win = do
-            quit <- windowShouldClose win
             pollEvents
             swapBuffers win
+
+            quit <- windowShouldClose win
             unless quit (loop win)
+
+        mousePos _win x y = print $ "mouse: x: " ++ (show x) ++ ";y: " ++ (show y)
