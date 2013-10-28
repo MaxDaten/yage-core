6a5c541e89e84774bc7b9f8e8fdd0cf0d3e0aea1
Jan-Philip Loos <maxdaten@gmail.com>
2013-10-29 00:19:14 +0100

    lens rename and predicates

--- src/Yage/Core/Application/EventTypes.hs
+++ src/Yage/Core/Application/EventTypes.hs
@@ -18,67 +18,67 @@ import           Graphics.UI.GLFW             as GLFWEventTypes ( FocusState(..)
 
 
 data EWindowPosition = EWindowPosition
-    { __winX             :: !Int
-    , __winY             :: !Int
+    { _winX             :: !Int
+    , _winY             :: !Int
     } deriving (Show, Ord, Eq, Typeable)
 makeLenses ''EWindowPosition
 
 data EWindowSize = EWindowSize
-    { __winWidth         :: !Int
-    , __winHeight        :: !Int
+    { _winWidth         :: !Int
+    , _winHeight        :: !Int
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EWindowSize
 
 data EWindowFocus = EWindowFocus
-    { __focusState       :: !FocusState }
+    { _focusState       :: !FocusState }
     deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EWindowFocus
 
 data EWindowIconify = EWindowIconify
-    { __iconifyState     :: !IconifyState }
+    { _iconifyState     :: !IconifyState }
     deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EWindowIconify
 
 data EFramebufferSize = EFramebufferSize
-    { __fbWidth          :: !Int
-    , __fbHeight         :: !Int
+    { _fbWidth          :: !Int
+    , _fbHeight         :: !Int
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EFramebufferSize
 
 data EMousePosition = EMousePosition
-    { __mousePosX        :: !Double
-    , __mousePosY        :: !Double
+    { _mousePosX        :: !Double
+    , _mousePosY        :: !Double
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EMousePosition
 
 data EMouseEnter = EMouseEnter
-    { __cursorState      :: !CursorState
+    { _cursorState      :: !CursorState
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EMouseEnter
 
 data EMouseButton = EMouseButton
-    { __mouseButton      :: !MouseButton
-    , __mouseButtonState :: !MouseButtonState
-    , __mouseModifier    :: !ModifierKeys
+    { _mouseButton      :: !MouseButton
+    , _mouseButtonState :: !MouseButtonState
+    , _mouseModifier    :: !ModifierKeys
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EMouseButton
 
 data EMouseScroll = EMouseScroll
-    { __mouseScrollX    :: !Double
-    , __mouseScrollY    :: !Double
+    { _mouseScrollX    :: !Double
+    , _mouseScrollY    :: !Double
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EMouseScroll
 
 data EKey = EKey
-    { __key             :: !Key
-    , __code            :: !Int
-    , __keyState        :: !KeyState
-    , __keyModifier     :: !ModifierKeys
+    { _key             :: !Key
+    , _code            :: !Int
+    , _keyState        :: !KeyState
+    , _keyModifier     :: !ModifierKeys
     } deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EKey
 
 data EChar = EChar
-    { __char            :: Char }
+    { _char            :: Char }
     deriving (Show, Ord, Eq, Typeable, Data)
 makeLenses ''EChar
 
@@ -100,3 +100,53 @@ data Event = EventError             !Error !String
            | EventKey               !GLFW.Window !EKey
            | EventChar              !GLFW.Window !EChar
            deriving (Typeable, Show, Ord, Eq)
+--------------------------------------------------------------------------------
+
+-- some predicates
+
+isEventError, isEventWindowPosition, isEventWindowSize, isEventWindowClose  :: Event -> Bool
+isEventWindowRefresh, isEventWindowFocus, isEventWindowIconify, isEventFramebufferSize  :: Event -> Bool 
+isEventMousePosition, isEventMouseEnter, isEventMouseButton, isEventMouseScroll :: Event -> Bool
+isEventKey, isEventChar :: Event -> Bool
+
+isEventError            EventError{}           = True
+isEventError _                                 = False
+
+isEventWindowPosition   EventWindowPosition{}  = True
+isEventWindowPosition _                        = False
+
+isEventWindowIconify    EventWindowIconify{}   = True
+isEventWindowIconify _                         = False
+
+isEventWindowSize       EventWindowSize{}      = True
+isEventWindowSize _                            = False
+
+isEventWindowClose      EventWindowClose{}     = True
+isEventWindowClose _                           = False
+
+isEventWindowRefresh    EventWindowRefresh{}   = True
+isEventWindowRefresh _                         = False
+
+isEventWindowFocus      EventWindowFocus{}     = True
+isEventWindowFocus _                           = False
+
+isEventFramebufferSize  EventFramebufferSize{} = True
+isEventFramebufferSize _                       = False
+
+isEventMousePosition    EventMousePosition{}   = True
+isEventMousePosition _                         = False
+
+isEventMouseEnter       EventMousePosition{}   = True
+isEventMouseEnter _                            = False
+
+isEventMouseButton      EventMouseButton{}     = True
+isEventMouseButton _                           = False
+
+isEventMouseScroll      EventMouseScroll{}     = True
+isEventMouseScroll _                           = False
+
+isEventKey              EventKey{}             = True
+isEventKey _                                   = False
+
+isEventChar             EventChar{}            = True
+isEventChar _                                  = False
